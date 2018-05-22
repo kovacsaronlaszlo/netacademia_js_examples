bd63083b461e6895aa15e35b0d86a6312a0d7f30
kovacs.aron <thahWeiJe7co>
2018-05-22 17:42:09 +0200

    [modify]: + add Card class some new fn + build game table

--- memóriajáték (osztályok-objektumok)/scripts.js
+++ memóriajáték (osztályok-objektumok)/scripts.js
@@ -1,6 +1,47 @@
+// const
+const gridSize = 4;
+
 // find elem
 let tableGrid = document.getElementById('grid');
 let myElement = document.getElementById('cell1');
-let card = new Card(myElement);
+let card = new Card(1, myElement);
+
+// init
+RenderGrid();
+
+// ================================================================== //
+
+function RenderGrid() {
+    tableGrid.innerHTML = '';
+
+    // card values collection
+    let cardValues = [];
+    for (let valueIndex = 1; valueIndex <= gridSize * gridSize / 2; valueIndex++) {
+        cardValues.push(valueIndex);
+        cardValues.push(valueIndex);
+    }
+    console.log(cardValues);
+
+    Shuffle(cardValues);
+
+    // rows
+    for (let rowIndex = 0; rowIndex < gridSize; rowIndex++) {
+        let newRow = document.createElement('tr');
+
+        // cells
+        for (let colIndex = 0; colIndex < gridSize; colIndex++) {
+            let newCell = document.createElement('td');
+            newCell.innerText = rowIndex + ':' + colIndex;
+            newRow.appendChild(newCell);
+        }
+        tableGrid.appendChild(newRow);
+    }
+}
+
+function Shuffle(collection) {
+    for (let i = collection.length; i >= 0; i--) {
+        console.log(i);
+    }
+}
 
 
--- memóriajáték (osztályok-objektumok)/src/card.js
+++ memóriajáték (osztályok-objektumok)/src/card.js
@@ -1,6 +1,27 @@
 class Card {
-    constructor(elem) {
+    constructor(value, elem) {
+        this.value = value;
         this.elem = elem;
+        this.color = 'black';
         console.log(this);
+
+        this.elem.addEventListener('click', (event) => {
+            this.OnClick();
+        });
+        this.UpdateColor();
+    }
+
+    OnClick() {
+        console.log('click');
+        this.ToggleColor();
+    }
+
+    ToggleColor() {
+        this.color = (this.color == 'black') ? 'white' : 'black';
+        this.UpdateColor();
+    }
+
+    UpdateColor() {
+        this.elem.style.backgroundColor = this.color;
     }
 }
