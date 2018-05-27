fdedf0a5b2889c4e9b3689fbccab1e0c07390185
kovacs.aron <thahWeiJe7co>
2018-05-27 20:11:07 +0200

    [create]: game table

--- snake/src/Board.js
+++ snake/src/Board.js
@@ -1,5 +1,24 @@
 class Board {
-    constructor() {
+    constructor(width, height) {
+        this.gridElement = document.getElementById('grid');
+        this.width = width;
+        this.height = height;
 
+        this.InitGrid();
+    }
+
+    InitGrid() {
+        for (let rowIndex = 0; rowIndex < this.height; rowIndex++) {
+            
+            let newRow = document.createElement('tr');
+            
+            for (let colIndex = 0; colIndex < this.width; colIndex++)  {
+
+                let newCell = document.createElement('td');
+                newRow.appendChild(newCell);
+            }
+
+            this.gridElement.appendChild(newRow);
+        }
     }
 }
--- snake/src/Game.js
+++ snake/src/Game.js
@@ -1,5 +1,11 @@
 class Game {
     constructor() {
+        // tábla
+        this.board = new Board(35, 15);
+
+        // kukac
+        this.worm = new Worm();
+
 
     }
 }
